38c73e0fdb82a8e15544802cc8dcd5e012d303be
unknown <drissaitkassi@gmail.com>
2023-03-01 09:35:18 +0000

    update html and componenet.ts for task-approbateur componenet to only show task asssigned to N3 validator

--- src/app/task-approbateur/task-approbateur.component.ts
+++ src/app/task-approbateur/task-approbateur.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl, FormGroup } from '@angular/forms';
+import { CamundaService } from '../services/camunda.service';
+import {Task} from "../interfaces/task"
 @Component({
   selector: 'app-task-approbateur',
   templateUrl: './task-approbateur.component.html',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskApprobateurComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private taskService:CamundaService ) { }
+  completeTaskForm! : FormGroup
+  requestBody! :object
+  taskList! :Array<Task>
   ngOnInit(): void {
+
+
+
+    this.completeTaskForm =new FormGroup({
+      value:new FormControl('')
+    })
+
+
+    this.handelTaskRetrival()
+
   }
 
+  public handelTaskRetrival(){
+    this.taskService.getTasksByAssignee('abdelghaffar').subscribe({
+      next:(data)=> {
+        this.taskList=data       },
+      error:(err)=>console.log("error")
+    })
+  }
+
+  handleTaskValidation(taskId :string,reqBody:object) {
+
+    this.requestBody={"variables":
+        {"taskVariable": this.completeTaskForm.value
+        }
+    }
+
+    this.taskService.completeTask(taskId,this.requestBody).subscribe({
+      next:(data)=> {
+        this.handelTaskRetrival()
+        console.log("sucesss")
+      },
+      error:(err)=>console.log("error completing task")
+    })
+
+  }
 }
